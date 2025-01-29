665e665fe0aa2106d94b519bc2868abcc8f665c0
AarushKumawat <28aarushk@gmail.com>
2025-01-29 22:28:04 +0530

    added Contact service class and Contact pojo class for production lvl code

--- src/main/java/com/first/webapp/controller/HomeController.java
+++ src/main/java/com/first/webapp/controller/HomeController.java
@@ -11,6 +11,9 @@ public class HomeController {
        
         return "home.html";
      }
+
+
+     
 }
  
 
---
+++ src/main/java/com/first/webapp/service/ContactService.java
@@ -0,0 +1,32 @@
+package com.first.webapp.service;
+
+import org.springframework.stereotype.Service;
+
+import com.first.webapp.model.Contact;
+
+import lombok.extern.slf4j.Slf4j;
+
+/*
+@Slf4j, is a Lombok-provided annotation that will automatically generate an SLF4J
+Logger static property in the class at compilation time.
+* */
+@Slf4j
+@Service
+public class ContactService {
+
+    /**
+     * Save Contact Details into DB
+     * @param contact
+     * @return boolean
+     */
+    
+    public boolean saveMessageDetails(Contact contact){
+        boolean isSaved = true;
+
+        //TODO - Need to persist the data into the DB table
+
+        log.info(contact.toString());
+        return isSaved;
+    }
+
+}
